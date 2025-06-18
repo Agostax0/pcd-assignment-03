0218a5417a673157ad49be63fb98c4f2b7a977e2
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-06-18 14:06:05 +0200

    boids update test

--- AkkaBoids/src/test/scala/BoidsModelTest.scala
+++ AkkaBoids/src/test/scala/BoidsModelTest.scala
@@ -1,7 +1,7 @@
 package it.unibo.pcd
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.must.Matchers.have
+import org.scalatest.matchers.must.Matchers.{have, not}
 import org.scalatest.matchers.should.Matchers.should
 
 class BoidsModelTest extends AnyFlatSpec:
@@ -15,3 +15,12 @@ class BoidsModelTest extends AnyFlatSpec:
     val model: BoidsModel = BoidsModel.empty.initBoids(100)
 
     model.boids should have size 100
+
+  it should "update boids correctly" in:
+    val model: BoidsModel = BoidsModel.empty.initBoids(100)
+
+    val boids = model.boids
+
+    val updatedModel = model.boids_=(boids.map(_.update(model)))
+
+    boids should not equal updatedModel.boids
