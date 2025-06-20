3a51c52bdbaf7c02819ecfd590264d8475b1ac10
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-06-20 15:21:33 +0200

    separation of Model into actor based model

---
+++ AkkaBoids/src/main/scala/ActorBoidsModel.scala
@@ -0,0 +1,40 @@
+package it.unibo.pcd
+
+import BoidsModel.LocalBoidsModel
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.Behaviors.*
+
+sealed trait BoidsModelMessages
+object BoidsModelMessages:
+  case class UpdateDimensions(width: Double, height: Double) extends BoidsModelMessages
+  case class UpdateNumberOfBoids(n: Int) extends BoidsModelMessages
+  case class UpdateParameters(separationWeight: Double, alignmentWeight: Double, cohesionWeight: Double)
+      extends BoidsModelMessages
+  case class Step(to: ActorRef[BoidsControllerMessages]) extends BoidsModelMessages
+
+object ActorBoidsModel:
+  class ActorBoidsModel:
+    def apply(
+        model: LocalBoidsModel = LocalBoidsModel()
+    ): Behavior[BoidsModelMessages] = receive: (context, message) =>
+      import BoidsModelMessages.*
+
+      val newModel = message match
+        case UpdateDimensions(width, height) =>
+          model.copy(width = width, height = height)
+        case UpdateNumberOfBoids(n) =>
+          model.copy(boids = model.initBoids(n))
+        case UpdateParameters(separationWeight, alignmentWeight, cohesionWeight) =>
+          model.copy(
+            separationWeight = separationWeight,
+            alignmentWeight = alignmentWeight,
+            cohesionWeight = cohesionWeight
+          )
+        case Step(to) =>
+          model.copy(boids = model.boids.map(_.update(model)))
+          to ! BoidsControllerMessages.GetData(model.boids)
+          model
+
+      apply(newModel)
+      same
--- AkkaBoids/src/main/scala/BoidsModel.scala
+++ AkkaBoids/src/main/scala/BoidsModel.scala
@@ -1,14 +1,25 @@
 package it.unibo.pcd
 
 import Boid.Boid
-import BoidsModelMessages.*
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+sealed trait BoidsModel:
+  val boids: Seq[Boid]
+  val separationWeight: Double
+  val alignmentWeight: Double
+  val cohesionWeight: Double
+  val width: Double
+  val height: Double
+  val maxSpeed: Double
+  val perceptionRadius: Double
+  val avoidRadius: Double
 
+  val minX: Double = -width / 2
+  val maxX: Double = width / 2
+  val minY: Double = -height / 2
+  val maxY: Double = height / 2
 object BoidsModel:
-  def actor: ActorBoidsModel = BoidsModel.ActorBoidsModel()
-
   case class LocalBoidsModel(
       boids: Seq[Boid] = List.empty,
       separationWeight: Double = 1.0,
@@ -19,60 +30,15 @@ object BoidsModel:
       maxSpeed: Double = 4.0,
       perceptionRadius: Double = 50.0,
       avoidRadius: Double = 20.0
-  ):
+  ) extends BoidsModel:
 
-    val minX: Double = -width / 2
-    val maxX: Double = width / 2
-    val minY: Double = -height / 2
-    val maxY: Double = height / 2
-
-    def initBoids(n: Int): LocalBoidsModel =
+    def initBoids(n: Int): Seq[Boid] =
       val random = scala.util.Random
-      copy(boids = for
+      for
         _ <- 0 until n
         pos = Position(random.between(minX, maxX), random.between(minY, maxY))
         vel = Velocity(random.between(0, maxSpeed / 2), random.between(0, maxSpeed / 2)) - Velocity(
           maxSpeed / 4,
           maxSpeed / 4
         )
-      yield Boid(pos, vel))
-
-  case class ActorBoidsModel():
-    def apply(
-        model: LocalBoidsModel = LocalBoidsModel(),
-        isRunning: Boolean = false
-    ): Behavior[BoidsModelMessages] = Behaviors.receive: (context, message) =>
-      message match
-        case UpdateDimensions(width, height) =>
-          val newModel = model.copy(width = width, height = height)
-          apply(newModel, isRunning)
-        case UpdateSeparationWeight(weight) =>
-          val newModel = model.copy(separationWeight = weight)
-          apply(newModel, isRunning)
-        case UpdateAlignmentWeight(weight) =>
-          val newModel = model.copy(alignmentWeight = weight)
-          apply(newModel, isRunning)
-        case UpdateCohesionWeight(weight) =>
-          val newModel = model.copy(cohesionWeight = weight)
-          apply(newModel, isRunning)
-        case StartSimulation() =>
-          context.log.info("Starting simulation...")
-          apply(model, true)
-        case StopSimulation() =>
-          context.log.info("Stopping simulation...")
-          apply(model)
-        case ResetSimulation() =>
-          context.log.info("Resetting simulation...")
-          val newModel = model.initBoids(model.boids.size)
-          apply(newModel)
-
-      Behaviors.same
-
-enum BoidsModelMessages:
-  case UpdateDimensions(width: Double, height: Double)
-  case UpdateSeparationWeight(weight: Double)
-  case UpdateAlignmentWeight(weight: Double)
-  case UpdateCohesionWeight(weight: Double)
-  case StartSimulation()
-  case StopSimulation()
-  case ResetSimulation()
+      yield Boid(pos, vel)
