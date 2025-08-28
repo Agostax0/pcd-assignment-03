3c47bfa59251d2a360a980cb78e67ef0da8d3231
Norbert Gabos <dont.vip@gmail.com>
2025-08-28 19:39:49 +0200

    Refactor GameInitializer for mass constants and AI

--- AkkaAgarIO/src/main/scala/it/unibo/agar/model/GameInitializer.scala
+++ AkkaAgarIO/src/main/scala/it/unibo/agar/model/GameInitializer.scala
@@ -7,24 +7,29 @@ import it.unibo.agar.model.Entity.Player
 import scala.util.Random
 
 object GameInitializer:
+  private val initialPlayerMass = 120.0
+  private val initialFoodMass = 100.0
+  
+  def spawnPlayer(id: String, width: Int, height: Int, initialMass: Double = initialPlayerMass): Player =
+    Player(id, Random.nextInt(width), Random.nextInt(height), initialMass)
 
-  def initialPlayers(numPlayers: Int, width: Int, height: Int, initialMass: Double = 120.0): Seq[Player] =
+  def initialPlayers(numPlayers: Int, width: Int, height: Int, initialMass: Double = initialPlayerMass): Seq[Player] =
     (1 to numPlayers).map[Player](i =>
       Player(NamePrefix.Player.toString + i, Random.nextInt(width), Random.nextInt(height), initialMass)
     )
 
-  def initialAIs(numAIs: Int, width: Int, height: Int, initialMass: Double = 120.0): Seq[Player] =
+  def initialAIs(numAIs: Int, width: Int, height: Int, initialMass: Double = initialPlayerMass): Seq[Player] =
     (1 to numAIs).map[Player](i =>
       Player(
         NamePrefix.AIPlayer.toString + i,
         Random.nextInt(width),
         Random.nextInt(height),
         initialMass,
-        Option(StupidAI)
+        Option(NearestFoodAI)
       )
     )
 
-  def initialFoods(numFoods: Int, width: Int, height: Int, initialMass: Double = 100.0): Seq[Food] =
+  def initialFoods(numFoods: Int, width: Int, height: Int, initialMass: Double = initialFoodMass): Seq[Food] =
     (1 to numFoods).map[Food](i =>
       Food(NamePrefix.Food.toString + i, Random.nextInt(width), Random.nextInt(height), initialMass)
     )
